aaca34ec370f218ee122450324b62fac3768d4dd
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:25:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int totalDays = int.Parse(Console.ReadLine());
+
+int years = totalDays / 365;
+int weeks = (totalDays % 365) / 7;
+int days = (totalDays % 365) % 7;
+
+Console.WriteLine($"{totalDays} ngay tuong duong voi:");
+Console.WriteLine($"{years} nam, {weeks} tuan va {days} ngay.");
