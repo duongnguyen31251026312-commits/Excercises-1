645f030cff80ccd4700fb7fda5d99c4d51d2d15b
ADMIN <ADMIN@DESKTOP-F9A069F>
2026-04-21 15:51:16 +0700

    Add project files.

---
+++ Excercises 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Nhap so thu nhat: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Nhap so thu hai: ");
+double b = Convert.ToDouble(Console.ReadLine());
+double sum = a + b;
+Console.WriteLine($"Tong cua {a} va {b} la: {sum}");
