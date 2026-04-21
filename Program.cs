a8e869a6dfd9983bc851edd7f4fdb7420041b988
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:19:24 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Nhap do Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius * 9 / 5) + 32;
+Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+Console.Write("Nhap do Fahrenheit: ");
+double f = Convert.ToDouble(Console.ReadLine());
+double c = (f - 32) * 5 / 9;
+Console.WriteLine($"{f}°F = {c}°C");
