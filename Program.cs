13489fddd1438fe7a0ef4db50fe7a60f4f065e5e
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:22:18 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Nhap mot ky tu: ");
+char character = Console.ReadKey().KeyChar;
+Console.WriteLine();
+int asciiValue = (int)character;
+Console.WriteLine($"Ma ASCII cua '{character}' la: {asciiValue}");
