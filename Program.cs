6c308882d795fd0898ff5545b1febda94bd74609
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:24:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Nhap do dai canh hinh vuong: ");
+double side = Convert.ToDouble(Console.ReadLine());
+double areaSquare = side * side;
+Console.WriteLine($"Dien tich hinh vuong la: {areaSquare}");
