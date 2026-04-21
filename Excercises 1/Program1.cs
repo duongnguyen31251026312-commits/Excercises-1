8cad8d6d91f7d8b501d703a2569c0e59ad7ae291
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:25:55 +0700

    Delete Excercises 1 directory

--- Excercises 1/Program1.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Write("Nhap so thu nhat: ");
-double a = Convert.ToDouble(Console.ReadLine());
-Console.Write("Nhap so thu hai: ");
-double b = Convert.ToDouble(Console.ReadLine());
-double sum = a + b;
-Console.WriteLine($"Tong cua {a} va {b} la: {sum}");
