c54c5b437c49db2dfe03121ae8b5632d06d1d7c7
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:21:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Kich thuoc cua int: " + sizeof(int) + " bytes");
+Console.WriteLine("Kich thuoc cua float: " + sizeof(float) + " bytes");
+Console.WriteLine("Kich thuoc cua double: " + sizeof(double) + " bytes");
+Console.WriteLine("Kich thuoc cua char: " + sizeof(char) + " bytes");
+Console.WriteLine("Kich thuoc cua bool: " + sizeof(bool) + " bytes");
