3a4ad80fb6825f75e41293361b585fd310bbaffe
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:15:46 +0700

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Nhap so thuc thu nhat: ");
+float n1 = float.Parse(Console.ReadLine());
+Console.Write("Nhap so thuc thu hai: ");
+float n2 = float.Parse(Console.ReadLine());
+float product = n1 * n2;
+Console.WriteLine($"Tich cua {n1} va {n2} la: {product}");
