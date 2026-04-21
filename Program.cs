54fe40cfbec16250c9696fc0bb533e3cd0406ef0
duongnguyen31251026312-commits <duongnguyen.31251026312@st.ueh.edu.vn>
2026-04-21 16:23:14 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Nhap ban kinh hinh tron: ");
+double r = Convert.ToDouble(Console.ReadLine());
+double areaCircle = Math.PI * Math.Pow(r, 2);
+Console.WriteLine($"Dien tich hinh tron la: {areaCircle:F2}");
